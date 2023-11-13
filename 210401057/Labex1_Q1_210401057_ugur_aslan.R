c79bfa5aa4840819abdf6c53470459bf69a3a88d
ugur aslan <ugurxaslan@gmail.com>
2023-11-13 23:18:11 +0300

    1.2, testleri ile tamamlandı

--- 210401057/Labex1_Q1_210401057_ugur_aslan.R
+++ 210401057/Labex1_Q1_210401057_ugur_aslan.R
@@ -1,5 +1,8 @@
-# 1.1
+#1.1
 download.file(
   "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv",
   destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv"
   )
+#1.2
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", sep=";" , header = TRUE)
+
--- 210401057/Labex1_Q1_tests_210401057_ugur_aslan.R
+++ 210401057/Labex1_Q1_tests_210401057_ugur_aslan.R
@@ -14,3 +14,11 @@ source("Labex1_Q1_210401057_ugur_aslan.R")
 test_that("MapsThatChangedOurWorld_StoryMap_Data.csv dosyası mevcut", {
   testthat::expect_true(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv"))
 })
+# 2.5
+test_that("maps adlı değiken Global Workspace’de mevcuttur", {
+  testthat::expect_true(exists("maps"))
+})
+# 2.6
+test_that("maps nesnesi bir data.frame’dir", {
+  testthat::expect_true(is.data.frame(maps))
+})
