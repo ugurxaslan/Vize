1badf9840322c0873f1cf30609f029c169d4988f
ugur aslan <ugurxaslan@gmail.com>
2023-11-13 20:24:15 +0300

    1.1, testleri ile tamamlandı

---
+++ 210401057/Labex1_Q1_210401057_ugur_aslan.R
@@ -0,0 +1,5 @@
+# 1.1
+download.file(
+  "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv",
+  destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv"
+  )
---
+++ 210401057/Labex1_Q1_tests_210401057_ugur_aslan.R
@@ -0,0 +1,16 @@
+library(testthat)
+#2.1 bu dosyayı oluşturdum
+
+#2.2
+rm(list = ls())
+if (file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")) {
+  file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+}
+
+# 2.3 
+source("Labex1_Q1_210401057_ugur_aslan.R")
+
+# 2.4
+test_that("MapsThatChangedOurWorld_StoryMap_Data.csv dosyası mevcut", {
+  testthat::expect_true(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv"))
+})
